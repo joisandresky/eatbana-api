6c2dbfa1b51cbbcd970eb61e02924868fbe1c1c7
Jois Andresky <jois@lighthouse.co.id>
2019-08-03 16:58:00 +0700

    add image upload

--- api/restaurant/index.js
+++ api/restaurant/index.js
@@ -1,12 +1,45 @@
 const express = require('express')
 const router = express.Router();
 const controller = require('./restaurant.controller');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+
+const upload = multer({
+  storage: storage, limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
+
 
 router.get('/', controller.index);
 router.get('/search', controller.search);
 router.get('/nearby', controller.nearby);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
+router.post('/upload', upload.single('image'), function (req, res) {
+  if (req.file) {
+    return res.status(200).json({ success: true, message: 'Image Uploaded', result: req.file });
+  } else {
+    return res.status(400).json({ success: false, message: 'Image not Uploaded!', result: req.file });
+  }
+});
+
 router.put('/:id', controller.update);
 router.delete('/:id', controller.destroy);
 
